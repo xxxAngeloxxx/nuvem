883870485076cfa5af9ee7df67ae44ed31fee1b9
ANGELO ANTONIO FERNANDES BARROS <20231198060021@ifrn.local>
2025-09-15 15:46:42 -0300

    Atualização para servidor

--- nuvem2/settings.py
+++ nuvem2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h44-$@u3j2qk$4nfv)gpqwpcpyvn#lg#w#k@*gj_teu*xx2guy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lalau.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

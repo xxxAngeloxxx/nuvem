f6fc4ebda7d9e01c093358914b49977cbff992a4
ANGELO ANTONIO FERNANDES BARROS <20231198060021@ifrn.local>
2025-09-22 15:42:45 -0300

    nada

--- nuvem2/urls.py
+++ nuvem2/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from core.views import index
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+   
 ]
